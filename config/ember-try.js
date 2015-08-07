00e2cdeddfab83966483169bee481ca457f68b20
Gastón I. Silva <givanse@gmail.com>
2015-08-07 03:13:07 -0500

    config ember-try ember-1.12.0

--- config/ember-try.js
+++ config/ember-try.js
@@ -5,6 +5,12 @@ module.exports = {
       dependencies: { }
     },
     {
+      name: 'ember-1.12.0',
+      dependencies: {
+        'ember': '1.12.0'
+      }
+    },
+    {
       name: 'ember-release',
       dependencies: {
         'ember': 'components/ember#release'
